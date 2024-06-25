4c9f79359dbd5057fd2d58e69a646a45e442ccb0
Caleb Joseph <calebbjoseph@gmail.com>
2024-06-25 15:02:10 -0500

    BBR-36 completed the Contact CRUD

--- routes/Web.js
+++ routes/Web.js
@@ -2,7 +2,11 @@ const express = require("express");
 const router = express.Router();
 const CheckAuthentication = require("../middlewares/CheckAuthentication");
 const CategoryController = require("../controllers/CategoryController");
+const ContactController = require("../controllers/ContactController");
 
+/**
+ * Category Routes
+ */
 // Create a new router for category routes
 const categoryRouter = express.Router();
 
@@ -14,5 +18,28 @@ categoryRouter.delete('/:id', CategoryController.destroy);
 
 // Use the category router for routes under /category
 router.use('/category', CheckAuthentication, categoryRouter);
+
+/**
+ * Category Routes Ends
+ */
+
+/**
+ * Category Routes
+ */
+// Create a new router for category routes
+const contactRouter = express.Router();
+
+contactRouter.post('/create', ContactController.create);
+contactRouter.get('/', ContactController.getAll);
+contactRouter.get('/:id', ContactController.getOne);
+contactRouter.delete('/:id', ContactController.destroy);
+
+// Use the category router for routes under /category
+router.use('/contact', contactRouter);
+
+/**
+ * Category Routes Ends
+ */
+
 
 module.exports = router;
