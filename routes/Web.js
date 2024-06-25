861d3af3f838cf72b1f54295f7cc8d0bbcd049be
Caleb Joseph <calebbjoseph@gmail.com>
2024-06-25 13:26:35 -0500

    added parent route

--- routes/Web.js
+++ routes/Web.js
@@ -1,13 +1,18 @@
-const router = require("express").Router();
-const AuthController = require("../controllers/AuthController");
+const express = require("express");
+const router = express.Router();
 const checkAuthentication = require("../middlewares/checkAuthentication");
 const CategoryController = require("../controllers/CategoryController");
 
+// Create a new router for category routes
+const categoryRouter = express.Router();
 
-router.post('/category/create', checkAuthentication, CategoryController.create);
-router.get('/category', checkAuthentication, CategoryController.getAll);
-router.get('/category/:id', checkAuthentication, CategoryController.getOne);
-router.put('/category/:id', checkAuthentication, CategoryController.update);
-router.delete('/category/:id', checkAuthentication, CategoryController.destroy);
+categoryRouter.post('/create', CategoryController.create);
+categoryRouter.get('/', CategoryController.getAll);
+categoryRouter.get('/:id', CategoryController.getOne);
+categoryRouter.put('/:id', CategoryController.update);
+categoryRouter.delete('/:id', CategoryController.destroy);
+
+// Use the category router for routes under /category
+router.use('/category', checkAuthentication, categoryRouter);
 
 module.exports = router;
