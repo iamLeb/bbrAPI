6dc0047530f8adb0b41c7aa38c43c0488168c0c5
Caleb Joseph <calebbjoseph@gmail.com>
2024-07-29 13:43:08 -0500

    added update

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -9,13 +9,13 @@ const createToken = (_id, res) => {
     // creating the token
     const token = jwt.sign({ _id }, process.env.SECRET, { expiresIn: '30d' });
 
+    console.log(token);
     // send the token to client cookie
     res.cookie('token', token, {
         httpOnly: true,
         secure: process.env.NODE_ENV === 'production', // Ensure this is true in production
         sameSite: 'None',
         expires: new Date(Date.now() + 8 * 3600000), // 8 hours
-        domain: 'bbr-client.netlify.app', // adjust as needed
         path: '/'
     });
 
@@ -56,7 +56,8 @@ const login = async (req, res) => {
         const user = await User.login(email, password);
 
         // create token
-        const token = createToken(user._id, res);
+        const token = await createToken(user._id, res);
+
         if (token) {
             return res.status(200).json(user);
         }
@@ -127,20 +128,20 @@ const update = async (req, res) => {
     }
 }
 
-// const logout = (req, res) => {
-//     try {
-//         res.clearCookie('token');
-//         res.status(200).json('You are logged out');
-//     } catch (e) {
-//         res.status(400).json({error: e.message});
-//     }
-// }
+const logout = (req, res) => {
+    try {
+        res.clearCookie('token');
+        res.status(200).json('You are logged out');
+    } catch (e) {
+        res.status(400).json({error: e.message});
+    }
+}
 
 module.exports = {
     register,
     login,
     checkAuth,
-    // logout,
+    logout,
     reset,
     update,
     agentregister
